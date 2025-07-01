5b5e9533074a7ba368319a4a2b70e5f99aae435d
Emre Küçük <kucukemrebey@gmail.com>
2025-07-01 21:52:50 +0300

    KDV tutarı erojesi eklendi

--- KDV Tutarı hesaplama/KdvTutari.java
+++ KDV Tutarı hesaplama/KdvTutari.java
@@ -1,13 +1,24 @@
 import java.util.Scanner;
 public class KdvTutari {
     public static void main(String[] args) {
-        int araba,ev,ucak,telefon;
         Scanner inp = new Scanner(System.in);
-        System.out.print("Araba fiyatını yazınız:");
-        araba = inp.nextInt();
-        int sonuc = araba + 25;
-        System.out.print("KDV'li fiyat:" + sonuc);
+        double tutar,kdvfiyati,kdvliTutar;
 
+        System.out.print("Ürünün fiyatını yazınız:");
+        tutar = inp.nextDouble();
+
+        double kdvOran =(tutar >= 0 && tutar <= 1000 ) ? 0.18 : 0.8 ;
+
+
+        kdvfiyati = tutar * kdvOran;
+        kdvliTutar = tutar + kdvfiyati;
+
+
+        System.out.println("KDV'siz fiyat" + tutar);
+        System.out.println("KDV oranı:" + kdvOran);
+        System.out.println("KDV'li fiyat:" + kdvliTutar);
+        System.out.print("KDV tutarı:" + kdvfiyati);
 
     }
+
 }
