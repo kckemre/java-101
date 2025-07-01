5f6cef62d888548bac18fd27af6c71211dff9259
Emre Küçük <kucukemrebey@gmail.com>
2025-07-01 14:11:03 +0300

    kdv tutarı

---
+++ KDV Tutarı hesaplama/KdvTutari.java
@@ -0,0 +1,8 @@
+import java.util.Scanner;
+public class KdvTutari {
+    public static void main(String[] args) {
+        int araba,ev,ucak,telefon;
+        ucak = 100;
+        System.out.print(ucak);
+    }
+}
